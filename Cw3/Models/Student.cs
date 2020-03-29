5b2d41cdd3813bb08d905049accb2bea04a67f2d
Daniel <s16597@pjwstk.edu.pl>
2020-03-29 20:23:17 +0200

    Commit

---
+++ Cw3/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Models
+{
+    public class Student
+    {
+
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string IndexNumber { get; set; }
+
+        public override string ToString()
+        {
+            return IdStudent + ", " + FirstName + ", " + Lastname + ", " + IndexNumber;
+        }
+    }
+}
